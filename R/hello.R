a52b6c84779da8c7e6a6178ddd8cf3b95d5ae7f3
KayTeeBee <kim-oliver-lausten@hotmail.com>
2018-09-24 21:12:28 +0200

    Small correction for plots.

--- R/hello.R
+++ R/hello.R
@@ -11,6 +11,12 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1
         j = j+1
       }
       plot(seq(1, maxshape, by = (maxshape-1)/steps1),corvec)
+      if (plots == TRUE){
+        hist(x, freq = FALSE, breaks = brks)
+        curve(dweibull(x, seq(minshape, maxshape, by=(maxshape-1)/steps1)[which.max(abs(corvec))]), col = "green", add = TRUE)
+        lines(density(x, bw = bandw, from = 0), col = "red")
+        legend("topleft", c("True Weibull density", "Estimated density"), lty = 2, col = c("green", "red"))
+      }
       cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
       cat("The best shape parameter is", seq(minshape, maxshape, by=(maxshape-1)/steps1)[which.max(abs(corvec))])
     } else {
@@ -22,14 +28,14 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1
         j = j+1
       }
       plot(seq(minshape, 1, by = (1-minshape)/steps2),corvec)
+      if (plots == TRUE){
+        hist(x, freq = FALSE, breaks = brks)
+        curve(dweibull(x, seq(minshape, maxshape, by=(1-minshape)/steps2)[which.max(abs(corvec))]), col = "green", add = TRUE)
+        lines(density(x, bw = bandw, from = 0), col = "red")
+        legend("topleft", c("True Weibull density", "Estimated density"), lty = 2, col = c("green", "red"))
+      }
       cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
       cat("The best shape parameter is", seq(minshape, maxshape, by=(1-minshape)/steps2)[which.max(abs(corvec))])
     }
-  }
-  if (plots == TRUE){
-    hist(x, freq = FALSE, breaks = brks)
-    curve(dweibull(x, seq(minshape, maxshape, by=steps)[which.max(corvec)]), col = "green", add = TRUE)
-    lines(density(x, bw = bandw, from = 0), col = "red")
-    legend("topleft", c("True Weibull density", "Estimated density"), lty = 2, col = c("green", "red"))
   }
 }
