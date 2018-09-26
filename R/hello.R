1780772636b5623ac95aa575db563729ecbaab43
KayTeeBee <kim-oliver-lausten@hotmail.com>
2018-09-26 18:28:46 +0200

    Added the normal dist and minor changes in poisson

--- R/hello.R
+++ R/hello.R
@@ -1,8 +1,8 @@
 # PPCC Plot
-ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1 = 10, steps2 = 10, plots = FALSE, brks = 70, meandist = FALSE, mean = NULL, bandw = 0.1, ...){
-  est <- density(x, bw = bandw, n = length(x), from = 0, to = max(x))$y
+ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1 = 10, steps2 = 10, plots = FALSE, brks = 70, meandist = FALSE, mean = NULL, bandw = 1, ...){
   if (distribution == "weibull"){
     j = 1
+    est <- density(x, bw = bandw, n = length(x), from = 0, to = max(x))$y
     if (minshape >= 1){
       corvec <- integer(steps1)
       for (i in seq(1, maxshape, by = (maxshape-1)/(steps1-1))){
@@ -39,9 +39,10 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1
     }
   } else if (distribution == "poisson"){
     j = 1
+    est <- density(x, bw = bandw, n = length(x), from = 0, to = max(x))$y
     corvec <- integer(steps1)
     for (i in seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))){
-      pois <- dpois(0:(length(x)-1), lambda = i)
+      pois <- dpois(round(seq(0, max(x), length = length(x))), lambda = i) ###ask ute bcs of evl bias when using round()?
       corvec[j] <- cor(est, pois)
       j = j+1
     }
@@ -54,5 +55,23 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1
     }
     cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
     cat("The best lambda is", seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))[which.max(abs(corvec))])
+  } else if (distribution == "normal"){
+    est <- density(x, bw = bandw, n = length(x),from = min(x), to = max(x))$y
+    j = 1
+    corvec <- integer(steps1)
+    for (i in seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))){
+      nor <- dnorm(seq(min(x), max(x), length = length(x)), mean = 0, sd = i)
+      corvec[j] <- cor(est, nor)
+      j = j+1
+    }
+    plot(seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), corvec)
+    if (plots == TRUE){
+      hist(x, freq = FALSE, breaks = brks)
+      curve(dnorm(x, mean = 0, sd = seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
+      lines(density(x, bw = bandw, from = min(x), to = max(x)), col = "red")
+      legend("topleft", c("True Normal density", "Estimated density"), lty = 2, col = c("green", "red"))
+    }
+    cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
+    cat("The best standard deviation is", seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))[which.max(abs(corvec))])
   }
 }
