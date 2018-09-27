198488d725ad24a85ddaa6aa483540ccb7a9977c
Kim <kim-oliver-lausten@hotmail.com>
2018-09-27 14:06:38 +0200

    Minor changes

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,5 @@
 # PPCC Plot
-ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1 = 10, steps2 = 10, plots = FALSE, brks = 70, meandist = FALSE, mean = NULL, bandw = 1, ...){
+ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1 = 10, steps2 = 10, plots = FALSE, brks = 70, meandist = FALSE, mean = NULL, bandw = 1, abc = "green", ...){
   if (distribution == "weibull"){
     j = 1
     est <- density(x, bw = bandw, n = length(x), from = 0, to = max(x))$y
@@ -10,9 +10,10 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1
         corvec[j] <- cor(est, wei)
         j = j+1
       }
-      plot(seq(1, maxshape, by = (maxshape-1)/(steps1-1)),corvec)
+      plot(seq(1, maxshape, by = (maxshape-1)/(steps1-1)), corvec, col = abc)
       if (plots == TRUE){
-        hist(x, freq = FALSE, breaks = brks)
+        a <- hist(x, freq = FALSE, breaks = brks)
+        hist(x, freq = FALSE, breaks = brks, ylim = c(0, (max(a$density))*1.5))
         curve(dweibull(x, seq(minshape, maxshape, by=(maxshape-1)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
         lines(density(x, bw = bandw, from = 0, to = max(x)), col = "red")
         legend("topleft", c("True Weibull density", "Estimated density"), lty = 2, col = c("green", "red"))
@@ -29,7 +30,8 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1
       }
       plot(seq(minshape, 1, by = (1-minshape)/(steps2-1)), corvec)
       if (plots == TRUE){
-        hist(x, freq = FALSE, breaks = brks)
+        a <- hist(x, freq = FALSE, breaks = brks)
+        hist(x, freq = FALSE, breaks = brks, ylim = c(0, (max(a$density))*1.5))
         curve(dweibull(x, seq(minshape, maxshape, by=(1-minshape)/(steps2-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
         lines(density(x, bw = bandw, from = 0, to = max(x)), col = "red")
         legend("topleft", c("True Weibull density", "Estimated density"), lty = 2, col = c("green", "red"))
@@ -48,26 +50,28 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 4, steps1
     }
     plot(seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), corvec)
     if (plots == TRUE){
-      hist(x, freq = FALSE, breaks = brks)
-      curve(dpois(x, seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
+      a <- hist(x, freq = FALSE, breaks = max(x))
+      hist(x, freq = FALSE, breaks = max(x), ylim = c(0, (max(a$density))*1.5))
+      lines(dpois(0:max(x), seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green")
       lines(density(x, bw = bandw, from = 0, to = max(x)), col = "red")
       legend("topleft", c("True Poisson density", "Estimated density"), lty = 2, col = c("green", "red"))
     }
     cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
     cat("The best lambda is", seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))[which.max(abs(corvec))])
   } else if (distribution == "normal"){
-    est <- density(x, bw = bandw, n = length(x),from = min(x), to = max(x))$y
+    est <- density(x, bw = bandw, n = length(x), from = min(x), to = max(x))$y
     j = 1
     corvec <- integer(steps1)
     for (i in seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))){
-      nor <- dnorm(seq(min(x), max(x), length = length(x)), mean = 0, sd = i)
+      nor <- dnorm(seq(min(x), max(x), length = length(x)), mean = mean(x), sd = i)
       corvec[j] <- cor(est, nor)
       j = j+1
     }
     plot(seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), corvec)
     if (plots == TRUE){
-      hist(x, freq = FALSE, breaks = brks)
-      curve(dnorm(x, mean = 0, sd = seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
+      a <- hist(x, freq = FALSE, breaks = brks)
+      hist(x, freq = FALSE, breaks = brks, ylim = c(0, (max(a$density))*1.5))
+      curve(dnorm(x, mean = mean(x), sd = seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
       lines(density(x, bw = bandw, from = min(x), to = max(x)), col = "red")
       legend("topleft", c("True Normal density", "Estimated density"), lty = 2, col = c("green", "red"))
     }
