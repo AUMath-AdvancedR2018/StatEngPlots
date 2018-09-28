0fbeef4cad2199de1a351609b8de11d2dc7c0a68
Kim <kim-oliver-lausten@hotmail.com>
2018-09-28 15:57:19 +0200

    Minor changes in the plot fcuntion and added a new feature to view the correlations.

--- R/ppcc.R
+++ R/ppcc.R
@@ -1,5 +1,5 @@
 # PPCC Plot
-ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 10, steps1 = 30, steps2 = 30, plots = FALSE, brks = 70, meandist = FALSE, mean = NULL, bandw = 1, abc = "green", ...){
+ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 10, steps1 = 30, steps2 = 30, plots = FALSE, brks = 70, bandw = "nrd0", returncor = FALSE){
   if (distribution == "weibull"){
     j = 1
     est <- density(x, bw = bandw, n = length(x), from = 0, to = max(x))$y
@@ -10,13 +10,17 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 10, steps
         corvec[j] <- cor(est, wei)
         j = j+1
       }
-      plot(seq(1, maxshape, by = (maxshape-1)/(steps1-1)), corvec, col = abc)
+      plot(seq(1, maxshape, by = (maxshape-1)/(steps1-1)), corvec)
+      if (returncor == TRUE){
+        df <- data.frame("Shape" = seq(1, maxshape, by = (maxshape-1)/(steps1-1)), "Correlation" = corvec)
+        print(df)
+      }
       if (plots == TRUE){
         a <- hist(x, freq = FALSE, breaks = brks)
         hist(x, freq = FALSE, breaks = brks, ylim = c(0, (max(a$density))*1.5))
         curve(dweibull(x, seq(minshape, maxshape, by=(maxshape-1)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
         lines(density(x, bw = bandw, from = 0, to = max(x)), col = "red")
-        legend("topleft", c("True Weibull density", "Estimated density"), lty = 2, col = c("green", "red"))
+        legend("topleft", c("True Weibull density", "Estimated density"), lty = 1, col = c("green", "red"))
       }
       cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
       cat("The best shape parameter is", seq(minshape, maxshape, by=(maxshape-1)/(steps1-1))[which.max(abs(corvec))])
@@ -29,12 +33,16 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 10, steps
         j = j+1
       }
       plot(seq(minshape, 1, by = (1-minshape)/(steps2-1)), corvec)
+      if (returncor == TRUE){
+        df <- data.frame("Shape" = seq(minshape, 1, by = (1-minshape)/(steps2-1)), "Correlation" = corvec)
+        print(df)
+      }
       if (plots == TRUE){
         a <- hist(x, freq = FALSE, breaks = brks)
         hist(x, freq = FALSE, breaks = brks, ylim = c(0, (max(a$density))*1.5))
         curve(dweibull(x, seq(minshape, maxshape, by=(1-minshape)/(steps2-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
         lines(density(x, bw = bandw, from = 0, to = max(x)), col = "red")
-        legend("topleft", c("True Weibull density", "Estimated density"), lty = 2, col = c("green", "red"))
+        legend("topleft", c("True Weibull density", "Estimated density"), lty = 1, col = c("green", "red"))
       }
       cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
       cat("The best shape parameter is", seq(minshape, maxshape, by = (1-minshape)/(steps2-1))[which.max(abs(corvec))])
@@ -48,13 +56,17 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 10, steps
       corvec[j] <- cor(est, pois)
       j = j+1
     }
+    if (returncor == TRUE){
+      df <- data.frame("Shape" = seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), "Correlation" = corvec)
+      print(df)
+    }
     plot(seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), corvec)
     if (plots == TRUE){
       a <- hist(x, freq = FALSE, breaks = max(x))
       hist(x, freq = FALSE, breaks = max(x), ylim = c(0, (max(a$density))*1.5))
       lines(dpois(0:max(x), seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green")
       lines(density(x, bw = bandw, from = 0, to = max(x)), col = "red")
-      legend("topleft", c("True Poisson density", "Estimated density"), lty = 2, col = c("green", "red"))
+      legend("topleft", c("True Poisson density", "Estimated density"), lty = 1, col = c("green", "red"))
     }
     cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
     cat("The best lambda is", seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))[which.max(abs(corvec))])
@@ -68,12 +80,16 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 10, steps
       j = j+1
     }
     plot(seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), corvec)
+    if (returncor == TRUE){
+      df <- data.frame("Shape" = seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), "Correlation" = corvec)
+      print(df)
+    }
     if (plots == TRUE){
       a <- hist(x, freq = FALSE, breaks = brks)
       hist(x, freq = FALSE, breaks = brks, ylim = c(0, (max(a$density))*1.5))
       curve(dnorm(x, mean = mean(x), sd = seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
       lines(density(x, bw = bandw, from = min(x), to = max(x)), col = "red")
-      legend("topleft", c("True Normal density", "Estimated density"), lty = 2, col = c("green", "red"))
+      legend("topleft", c("True Normal density", "Estimated density"), lty = 1, col = c("green", "red"))
     }
     cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
     cat("The best standard deviation is", seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))[which.max(abs(corvec))])
@@ -87,12 +103,16 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 10, steps
       j = j+1
     }
     plot(seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), corvec)
+    if (returncor == TRUE){
+      df <- data.frame("Shape" = seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), "Correlation" = corvec)
+      print(df)
+    }
     if (plots == TRUE){
       a <- hist(x, freq = FALSE, breaks = brks)
       hist(x, freq = FALSE, breaks = brks, ylim = c(0, (max(a$density))*1.5))
       curve(dexp(x, seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
       lines(density(x, bw = bandw, from = 0, to = max(x)), col = "red")
-      legend("topleft", c("True Exponential density", "Estimated density"), lty = 2, col = c("green", "red"))
+      legend("topleft", c("True Exponential density", "Estimated density"), lty = 1, col = c("green", "red"))
     }
     cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
     cat("The best lambda is", seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))[which.max(abs(corvec))])
@@ -107,12 +127,16 @@ ppcc <- function(x, distribution = "weibull", minshape = 1, maxshape = 10, steps
       j = j+1
     }
     plot(seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), corvec)
+    if (returncor == TRUE){
+      df <- data.frame("Shape" = seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1)), "Correlation" = corvec)
+      print(df)
+    }
     if (plots == TRUE){
       a <- hist(x, freq = FALSE, breaks = brks)
       hist(x, freq = FALSE, breaks = brks, ylim = c(0, (max(a$density))*1.5))
       curve(dnorm(x, mean = mean(x), sd = seq(minshape, maxshape, by=(maxshape-minshape)/(steps1-1))[which.max(abs(corvec))]), col = "green", add = TRUE)
       lines(density(x, bw = bandw, from = min(x), to = max(x)), col = "red")
-      legend("topleft", c("True Log-Normal density", "Estimated density"), lty = 2, col = c("green", "red"))
+      legend("topleft", c("True Log-Normal density", "Estimated density"), lty = 1, col = c("green", "red"))
     }
     cat("The highest absolute correlation coefficient is", max(abs(corvec)), "\n")
     cat("The best standard deviation is", seq(minshape, maxshape, by = (maxshape-minshape)/(steps1-1))[which.max(abs(corvec))])
